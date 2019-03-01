a7f45f1bcd7457fe97b227a21e89b8a82cc5fa49
Chelsea Finn <cbfinn@eecs.berkeley.edu>
2019-02-28 17:00:01 -0800

    update usage instructions to match paper

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ Usage Instructions:
         python main.py --datasource=sinusoid --logdir=logs/sine/ --pretrain_iterations=70000 --metatrain_iterations=0 --norm=None --update_batch_size=10
 
     5-way, 1-shot omniglot:
-        python main.py --datasource=omniglot --metatrain_iterations=40000 --meta_batch_size=32 --update_batch_size=1 --update_lr=0.4 --num_updates=1 --logdir=logs/omniglot5way/
+        python main.py --datasource=omniglot --metatrain_iterations=60000 --meta_batch_size=32 --update_batch_size=1 --update_lr=0.4 --num_updates=1 --logdir=logs/omniglot5way/
 
     20-way, 1-shot omniglot:
-        python main.py --datasource=omniglot --metatrain_iterations=40000 --meta_batch_size=16 --update_batch_size=1 --num_classes=20 --update_lr=0.1 --num_updates=5 --logdir=logs/omniglot20way/
+        python main.py --datasource=omniglot --metatrain_iterations=60000 --meta_batch_size=16 --update_batch_size=1 --num_classes=20 --update_lr=0.1 --num_updates=5 --logdir=logs/omniglot20way/
 
     5-way 1-shot mini imagenet:
         python main.py --datasource=miniimagenet --metatrain_iterations=60000 --meta_batch_size=4 --update_batch_size=1 --update_lr=0.01 --num_updates=5 --num_classes=5 --logdir=logs/miniimagenet1shot/ --num_filters=32 --max_pool=True
@@ -22,6 +22,8 @@ Usage Instructions:
     To run evaluation, use the '--train=False' flag and the '--test_set=True' flag to use the test set.
 
     For omniglot and miniimagenet training, acquire the dataset online, put it in the correspoding data directory, and see the python script instructions in that directory to preprocess the data.
+
+    Note that better sinusoid results can be achieved by using a larger network.
 """
 import csv
 import numpy as np
